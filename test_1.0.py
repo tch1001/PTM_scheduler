ebb4e3faf283171bf647bddd60b33310e08f6a27
tch1001 <tanchienhao@gmail.com>
2017-06-09 17:01:25 +0800

    deleted unnecessary old files

--- test_1.0.py
+++
@@ -1,9 +0,0 @@
-import scheduler
-import generator
-
-slots = 12
-people = 10 #starts waiting for really long after 20
-ava = [i for i in range(1,slots+1)]
-print(scheduler.allocate(ava, generator.allCanMakeIt(people), ava)) #ava as the priority list as well
-print(scheduler.allocate(ava, generator.someCanMakeIt(people), ava)) #ava as the priority list as well
-print(scheduler.allocate(ava, generator.noneCanMakeIt(people), ava)) #ava as the priority list as well
